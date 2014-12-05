289d6a80a3bfc0a837e3553a11b59da9da4b7326
Pancham Mehrunkar <pancham348@gmail.com>
2014-12-04 20:37:32 -0800

    fixed user sign up bugs

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,7 +11,7 @@ module Api
       @user = User.new(user_params)
       if @user.save
         sign_in(@user)
-        #redirect_to root_url
+        redirect_to "#"
       else
         flash.now[:errors] = @user.errors.full_messages
         render :new
